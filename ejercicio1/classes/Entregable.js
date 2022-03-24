52bb5243ce14f0a16ff4ecb369b6ed282b1582e9
ripterdust <bryantello2010@hotmail.com>
2022-03-24 16:26:12 -0600

    Added the main methods to the Entregable file

--- ejercicio1/classes/Entregable.js
+++ ejercicio1/classes/Entregable.js
@@ -1,3 +1,12 @@
-class Entregable {}
+class Entregable {
+    #prestado;
+    entregar() {
+        this.#prestado = true;
+    }
+    devolver() {
+        this.#prestado = false;
+    }
+    isEntregado() {}
+}
 
 module.exports = Entregable;
